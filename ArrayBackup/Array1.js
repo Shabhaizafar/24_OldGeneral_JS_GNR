9a013026477fe4ae4ced19cd7a6482e1b6fc57c1
Shabhai Zafar <zafarshabhai03@gmail.com>
2024-09-29 17:52:27 +0530

    Array2

--- ArrayBackup/Array1.js
+++ ArrayBackup/Array1.js
@@ -147,26 +147,167 @@
 // console.log("Value of Arr1 : ",Arr1);
 // console.log("Value of Arr2 : ",Arr2);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 //////////////////////////////////////////////////////
+
 // ✅ Clone array & spread operator.
-// ✅ For loop.
+
+// clone  : duplicate + additional
+// copy : same to same
+
+// Whatsapp : 
+    // chat 
+    // status
+    // Ai 
+    // group creation
+    // video/audio call
+    // hide last seen
+
+
+
+// GB Whatsapp :
+    // chat 
+    // status
+    // Ai 
+    // group creation
+    // video/audio call
+    // status download
+    // stop last seen
+        // 12 spe,2023
+
+//Copy : 
+// var Arr1 = [1,2,3];
+// var Arr2 = Arr1;
+// console.log("Before : ");
+// console.log("Value of Arr1 : ",Arr1);
+// console.log("Value of Arr2 : ",Arr2);
+// Arr1.push(100);
+// Arr2.push("Royal");
+// console.log("After : ");
+// console.log("Value of Arr1 : ",Arr1);
+// console.log("Value of Arr2 : ",Arr2);
+
+
+// Clone :  
+// 1) using concat : 
+// var Arr1 = [1,2,3];
+// var Arr2 = Arr1.concat(1000,2000);
+// console.log("Before : ");
+// console.log("Value of Arr1 : ",Arr1);
+// console.log("Value of Arr2 : ",Arr2);
+// Arr1.push(100);
+// Arr2.push("NewOne");
+// console.log("After : ");
+// console.log("Value of Arr1 : ",Arr1);
+// console.log("Value of Arr2 : ",Arr2);
+
+// 2) using slice :
+// var Arr1 = [1,2,3,4,5,6,7];
+// var Arr2 = Arr1.slice();
+// var Arr2 = Arr1.slice(3);
+// var Arr2 = Arr1.slice(3,6);
+// console.log("Before : ");
+// console.log("Value of Arr1 : ",Arr1);
+// console.log("Value of Arr2 : ",Arr2);
+// Arr1.push(100);
+// Arr2.push("NewOne");
+// console.log("After : ");
+// console.log("Value of Arr1 : ",Arr1);
+// console.log("Value of Arr2 : ",Arr2);
+
+// 3) using Spread Operator :  Syntax : (3 Dots) ...
+// var Arr1 = [1,2,3];
+// var Arr2 = [...Arr1];
+// console.log("Before : ");
+// console.log("Value of Arr1 : ",Arr1);
+// console.log("Value of Arr2 : ",Arr2);
+// Arr1.push(100);
+// Arr2.push("NewOne");
+// console.log("After : ");
+// console.log("Value of Arr1 : ",Arr1);
+// console.log("Value of Arr2 : ",Arr2);
+
+///////////////////////////////////////////////////////
 // ✅ use const for creating arrays.
+// if : var 
+// var Arr = [1,2,3];
+// console.log(Arr);
+// Arr= [];
+// var Arr = ["Zafar","Royal"];
+// console.log(Arr);
+
+// if : let  
+// let Arr = [1,2,3];
+// console.log(Arr);
+// let Arr = []; //error
+// Arr = [];
+// console.log(Arr);
+
+
+//if : const 
+// const Arr = [1,2,3];
+// console.log(Arr);
+// const Arr = []; //error
+// Arr = []; //error
+
+
+// const Arr = [1,2,3];
+// console.log(Arr);
+// Arr.push(1000);
+// console.log(Arr);
+
+
+
+/////////////////////////////////////////////
+// ✅ For loop.
 // ✅ While loop in array.
 // ✅ For of loop.
 // ✅ For in loop.
+
+const Arr = [11,12,13,14,15,"Zafar","Royal"];
+//            0  1  2  3  4    ....................
+// Length Property : 
+// console.log(Arr.length);
+
+// xyz.sdfg()   :Method 
+// xyz.sdfg     :Property 
+
+// Loop in a Array : 
+// 1) Basic For Loop  : 
+//  i : index  starting always :  0 
+// for (let i = 0; i < Arr.length; i++) {
+//     console.log(i,Arr[i]);
+// }
+
+
+// 2) while Loop 
+// let i = 0;
+// while (i<Arr.length) {
+//     console.log(i,Arr[i]);
+//     i++;
+// }
+
+// 3) do-while Loop
+// let i = 0;
+// do{
+//     console.log(i,Arr[i]);
+//     i++;
+// }while(i<Arr.length);
+
+// 4) for-in Loop   : key(similer to index) and value  
+// for (const key in Arr) {
+//     console.log(key,typeof key,Arr[key]);
+// }
+
+// 5) for-of Loop :  value
+// for (const element of Arr) {
+//     console.log(element);
+// }
+
+// 6) foreach Loop : 
+        //  value index
+// Arr.forEach((abc,xyz) => {
+//    console.log(abc,xyz);
+// });
+
+////////////////////////////////////
 // ✅ Array destructuring.
