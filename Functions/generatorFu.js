b6816a783e1cab9eb93c5ef326d5b3ab6581d6e7
Shabhaizafar <zafarshabhai03@gmail.com>
2024-10-22 19:35:53 +0530

    Function

---
+++ Functions/generatorFu.js
@@ -0,0 +1,56 @@
+// Generator Function : 
+// Syntax : 
+/*
+valid method :
+1) function * functionname(){
+
+}
+
+2) function* functionname(){
+
+}
+
+3) function *functionname(){
+
+}
+
+4) function*functionname(){
+
+}
+*/
+
+function *Generatorfu(){
+    yield "Hello,Everyone !!";
+    yield "Welcome to Javascript !!";
+    yield "Have a Good Day.";
+    yield "Bye Bye";
+}
+
+var functionrunner = Generatorfu();
+
+// next method  :   
+
+// console.log(functionrunner.next().value); //Hello,Everyone !!
+// console.log(functionrunner.next().value); //Welcome to Javascript !!
+// console.log(functionrunner.next().value); //Have a Good Day.
+// console.log(functionrunner.next().value); //Bye Bye
+// console.log(functionrunner.next().value); //undefined
+
+
+console.log(functionrunner.next());
+console.log(functionrunner.next());
+console.log(functionrunner.next());
+console.log(functionrunner.next());
+console.log(functionrunner.next());
+
+
+
+// Create a generator that yields the characters of a given string one by one.
+
+
+// "Zafar"
+// Z
+// a
+// f 
+// a 
+// r 
