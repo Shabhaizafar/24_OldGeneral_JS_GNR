ba78de0590a46d6c9343751ed1f9a62fb26a17a1
Shabhaizafar <zafarshabhai03@gmail.com>
2024-11-12 18:54:11 +0530

    Array Method's

---
+++ Array2/ArrayMethods.js
@@ -0,0 +1,57 @@
+const Arr = [11,12,13,14,15,16,17];
+console.log(Arr);
+
+//Function : 
+// => Foreach method. : 
+// - work as a loop.
+// - there are 3 perameters
+    // value,index,self Array
+// - no return   
+// Arr.forEach(
+//     function (values,index,selfArr){
+//         // console.log(values,index,selfArr);
+//     }
+// );
+///////////////
+// => Foreach method : 
+console.log("Foreach method : ");
+var ans = Arr.forEach(function (values,index,selfArr){
+    // console.log(values,index,selfArr);
+    if(values>13){
+        return values;
+    }
+});
+console.log(ans);
+// - map method.
+console.log("Map method : ");
+// return Same Array : different Value 
+var ans2 = Arr.map(function (values,index,selfArr){
+    // console.log(values,index,selfArr);
+    if(values>13){
+        return values;
+    }
+});
+console.log(ans2);
+
+
+// - Filter.
+console.log("Filter method : ");
+// filter value return 
+var ans3 = Arr.filter(function (values,index,selfArr){
+    // console.log(values,index,selfArr);
+    if(values>13){
+        return values;
+    }
+});
+console.log(ans3);
+
+
+// console.log(ans);
+// - Sort.
+// - Find.
+// - Every.
+// - Some.
+// - Fill method.
+// - Splice method.
+// - Reduce.
+
---
+++ Functions/callback.js
@@ -0,0 +1,21 @@
+// var i = 12;
+// function Main(callback){
+        //          abc
+//     console.log("Hello Everyone!!");
+    // callback();
+    // abc();
+    // All(); 
+// }
+// Main(All);
+
+
+// function All(){
+//     console.log("Second Function!!",i++);
+// }
+
+// select * from employees; 
+// table  : 12
+// select  salary*12 as NewSalary from employees;
+// column + 1
+// select * from employees; 
+// table : 12
