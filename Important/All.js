59d7f76ede8584311e10d932d43eadcdfa8a2ffb
Shabhaizafar <zafarshabhai03@gmail.com>
2024-11-19 18:39:51 +0530

    Iterable,Set

---
+++ Important/All.js
@@ -0,0 +1,96 @@
+// - Iterables : index 
+
+// var str = "Royal";
+// var arr = ["Royal","Technosoft","pvt","Ltd"];
+
+// console.log(str);
+// console.log("Basic For Loop :");
+// for (let i = 0; i < str.length; i++) {
+//     console.log(str[i]);
+// }
+// console.log("For of Loop :");
+// for (const element of str) {
+//     console.log(element);
+// }
+
+// console.log(arr);
+// console.log("Basic For Loop :");
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+// }
+// console.log("For of Loop :");
+// for (const element of arr) {
+//     console.log(element);
+// }
+
+
+// var obj = {
+//     fname  : " Raj",
+//     lname : "Shah",
+//     Age : 30
+// };
+// console.log(obj);
+// console.log("Basic For Loop :");
+// for (let i = 0; i < obj.length; i++) {
+//     console.log(arr[i]);
+// }
+// console.log("For of Loop :");
+// for (const element of obj) {
+//     console.log(element);
+// }
+
+// console.log("For in Loop :");
+// for (const key in obj) {
+//     console.log(key,obj[key]);
+// }
+
+// object is not iterable
+// array and string are iterable.
+
+///////////////////////////////////////////////////////////////
+// - Sets.
+
+// var mySet = new Set([11,12,13,12,13]);
+// console.log(mySet);
+
+// console.log(mySet[0]);
+// console.log(typeof mySet);
+
+// for (let i = 0; i < mySet.length; i++) {
+//     console.log(mySet[i]);
+// }
+// for (const element of mySet) {
+//     console.log(element);
+// }
+// for (const element in mySet) {
+//     console.log(element);
+// }
+// Array : allowed  duplicate values
+//         Index available
+//         Iterables
+
+
+// Set  : allowed  duplicate values
+//         Index not available
+//         not Iterables
+
+
+
+
+
+
+///////////////////////////////////////////////////////////////
+
+// - Maps.
+// - Object.assign.
+// - Optional chaining.
+
+
+//////////////////////////////////////////////////////
+
+
+// Reverse an Array Using an Iterable: Write a program that reverses an array by iterating through it backward using an iterable approach.
+
+// Create an Iterable Range: Write a custom iterable that generates a range of numbers between two given numbers 
+
+// Remove Duplicates from an Array: Use an iterable method (e.g., Set) to remove duplicate elements from an array.
