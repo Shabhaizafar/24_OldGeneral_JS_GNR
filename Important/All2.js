b10d554e96cc60847d1a322304db396512cbd58a
Shabhaizafar <zafarshabhai03@gmail.com>
2024-11-23 19:56:23 +0530

    Map

---
+++ Important/All2.js
@@ -0,0 +1,109 @@
+// set  : 
+
+// var mySet = new Set([11,12,13,14,11,12]);
+
+// console.log(mySet);
+// console.log(mySet[0]);
+
+
+// Set  : allowed  duplicate values
+//         Index not available
+//         not Iterables
+        // only single Argument
+// mySet.add(100);
+
+// empty full set 
+// mySet.clear();
+
+// delete specific Number
+// mySet.delete(12);
+
+
+// print key values pair
+// console.log(mySet.entries());
+
+
+// console.log(mySet.keys());
+// console.log(mySet.values());
+
+
+// print total number available in Set.
+// console.log(mySet.size);
+
+
+// check value exist or Not 
+// console.log(mySet.has(110));
+
+// console.log(mySet);
+
+// mySet.forEach((v,a,b)=>{
+//     console.log(v,a,b);
+// });
+
+// var Arr = [11,12,13,14];
+
+// Arr.forEach((v,a,b)=>{
+//     console.log(v,a,b);
+// });
+//////////////////////////////////////////
+// Map : 
+// key : value  pair 
+
+
+// var mySet = new Set();
+// console.log(mySet);
+
+// var obj = {
+//     Fname : "Raj",
+//     Lname : "Shah",
+//     1     : 123
+// }
+
+// console.log(obj);
+
+// var myMap = new Map();
+// console.log(myMap);
+
+// // Add a new Property in a Map : 
+// myMap.set("Fname","Raj");
+// myMap.set("Lname","Shah");
+// myMap.set(1,123);
+
+// console.log(myMap);
+
+
+// /////////////////////////////////////////
+// console.log("OBJ : ");
+// for (const key in obj) {
+//     console.log(key,typeof key);
+// }
+// console.log("MAP : ");
+
+// for (const key1 of myMap) {
+//     console.log(key1[0],typeof key1[0]);
+// }
+
+
+// Obj : for..of  loop not workable
+    //  error
+
+    //  all keys  : String
+
+// map : for..in  loop not workable
+    //  not showing error
+
+    //  Keys : different type available
+
+
+
+
+// diffrence Between object and Set.
+// diffrence Between array and Set.
+
+// diffrence Between object and Map.
+// diffrence Between Array and Map.
+
+// diffrence Between Map and Set.
+
+
+// 7 to 8 
