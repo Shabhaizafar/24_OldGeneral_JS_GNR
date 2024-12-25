2a5a34fd9f326fc3e74aa74953166d74335b26e1
Shabhaizafar <zafarshabhai03@gmail.com>
2024-12-25 19:30:31 +0530

    DOM1

---
+++ DOM/dom1.js
@@ -0,0 +1,34 @@
+// document Object 
+// console.log(document);
+
+
+//body 
+// console.log(document.body);
+
+//head
+// console.log(document.head);
+
+
+// not accessable link This
+// console.log(document.head.title);
+
+
+
+// 1) using getElementById():
+console.log(document.getElementById('heading1'));
+
+// 2) using getElementByTagName():
+console.log(document.getElementsByTagName('h3')[0]);
+console.log(document.getElementsByTagName('h3')[1]);
+
+
+// 3) using getElementByClassname():
+console.log(document.getElementsByClassName('heading2')[0]);
+console.log(document.getElementsByClassName('heading2')[1]);
+
+
+
+// 4) using querySelector():
+// 5) using querySelectorAll();
+
+
