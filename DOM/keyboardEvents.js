564f352be4f809de54266d355dc6b261df664115
Shabhaizafar <zafarshabhai03@gmail.com>
2025-01-18 18:49:40 +0530

    Type

---
+++ DOM/keyboardEvents.js
@@ -0,0 +1,20 @@
+// const body = document.body;
+// const div = document.querySelector("div");
+
+// onkeyup
+// onkeydown
+// onkeypress
+
+// var s = "";
+// body.onkeypress = function(event){
+//     // console.log(event);
+//     if(event.key == " "){
+//         s= " ";
+//     }else{
+//         div.innerText += s+event.key;
+//         s = "";
+//     }
+// }
+
+
+
