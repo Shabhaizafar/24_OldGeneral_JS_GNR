b69535e4c61da392ad9fdbf774eca55e34036e20
Shabhaizafar <zafarshabhai03@gmail.com>
2025-02-11 17:40:20 +0530

    event bubbling vs event capturing vs event delegation

---
+++ DOM/add.js
@@ -0,0 +1,68 @@
+// event bubbling 
+// const body = document.body;
+// const div = document.querySelector('div');
+// const h1 = document.querySelector('h1');
+
+// body.addEventListener('click',function(){
+//     console.log("Body !!!!!");
+// });
+
+// div.addEventListener('click',function(){
+//     console.log("Div !!!!!");
+// });
+
+// h1.addEventListener('click',function(){
+//     console.log("h1 !!!!!");
+// });
+
+
+
+// event capturing 
+// const body = document.body;
+// const div = document.querySelector('div');
+// const h1 = document.querySelector('h1');
+
+
+// body.addEventListener('click',function(){
+//     console.log("Body !!!!!");
+// },true);
+
+// div.addEventListener('click',function(){
+//     console.log("Div !!!!!");
+// },true);
+
+// h1.addEventListener('click',function(){
+//     console.log("h1 !!!!!");
+// });
+
+
+
+// const body = document.body;
+// const div = document.querySelector('div');
+// const h1 = document.querySelector('h1');
+// var a = 0;
+
+
+// body.addEventListener('click',function(){
+//     // console.log("Body !!!!!");
+//     a = "Body";
+// },true);
+
+// div.addEventListener('click',function(){
+//     // console.log("Div !!!!!");
+//     a = "Div";
+// },true);
+
+// h1.addEventListener('click',function(){
+//     // console.log("h1 !!!!!");
+//     console.log(a);
+// });
+
+
+
+// event delegation
+const body = document.body;
+
+body.addEventListener('click',function(event){
+    console.log(event.target);
+});
