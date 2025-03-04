0165ae3265c19fa774bbd7e544bb06574ab55001
Shabhaizafar <zafarshabhai03@gmail.com>
2025-03-04 17:40:45 +0530

    worker

---
+++ DOM/work.js
@@ -0,0 +1,15 @@
+// for (let i = 0; i <10; i++) {
+//     console.log(i+1);
+// }
+
+// var i=0;
+
+// setInterval(() => {
+//     return i;
+//     i++;
+// }, 1000);
+
+function sayHello(){
+    return "Hello";
+}
+sayHello();
---
+++ DOM/worker.js
@@ -0,0 +1,22 @@
+// const btn = document.querySelector("button");
+// var w;
+// btn.addEventListener('click',function(){
+//     if(typeof(w)=='undefined'){
+//         w =new Worker('work.js');
+//     }
+// });
+
+
+
+
+
+// argument remaining : 
+
+// const btn = document.querySelector("button");
+// const div = document.querySelector('div');
+// var w;
+// btn.addEventListener('click',function(){
+//     if(typeof(w)=='undefined'){
+//         w =new Worker('work.js');
+//     } 
+// });
