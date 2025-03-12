5fb9d94032239f8d81b5a218c4920cd40ed07699
Shabhai Zafar <zafarshabhai03@gmail.com>
2025-03-12 16:34:15 +0530

    Promise

---
+++ Promise.js
@@ -0,0 +1,30 @@
+// var a = 1;
+// var myPromise = new Promise((success,resolve)=>{
+//     if(a>10){
+//         success();
+//     }else{
+//         resolve();
+//     }
+// });
+
+// myPromise.then(()=>{
+//     console.log("Success!!");
+// },()=>{
+//     console.log("Reject!!");
+// });
+
+
+// Write a JavaScript function that takes a callback and invokes it after a delay of 2 second.
+
+// var myPromise2 =  new Promise((success,resolve)=>{
+//     setTimeout(() => {
+//         success();
+//     }, 2000);
+//     // resolve();
+// });
+
+// myPromise2.then(()=>{
+//     console.log("Success");
+// },()=>{
+//     console.log("Reject");
+// });
